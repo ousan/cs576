9cce52587432db796c999fd1f6281bc596af61b4
cagridurak <cagri.durak@vestel.com.tr>
2016-10-31 13:40:35 +0200

    Update ContactModel.java
    contact model has been added.

--- ContactModel.java
+++ ContactModel.java
@@ -5,5 +5,44 @@ import java.util.Locale;
 
 public class ContactModel {
 
-
+	private String searchData;
+	private String searchType;
+	
+	public void openAndSearchDatabase() throws SQLException{
+	    Connection conn=DriverManager.getConnection("jdbc:ucanaccess://rehber.MDB");
+		Statement s = conn.createStatement();
+		System.out.println("Opened database successfully");
+		Locale trlocale = new Locale("en-US");
+		String query;
+		if(searchType == "name")
+		{
+			query ="select * from fihrist where field1 like \"*"+ searchData +"*\"";
+		    System.out.println(query);	
+		}
+		else
+		{
+			query ="select * from fihrist where field2 like \"*"+ searchData +"*\"";
+		    System.out.println(query);
+		}
+		
+		ResultSet rs = s.executeQuery(query);
+		while (rs.next()) {
+			/*
+		    System.out.print(rs.getString(1) + "| |");
+		    System.out.print(rs.getString(2) + "| |");
+		    System.out.println(rs.getString(3)+ "|");
+		    */
+			System.out.println(String.format("%s %s %s",rs.getString(1),rs.getString(2),rs.getString(3)));
+		}
+		System.out.println("query finished");
+		rs.close();
+		s.close();
+	}
+	public void setSearchData(String search_data){
+		this.searchData = search_data;
+	}
+	
+	public void setSearchType(String search_type){
+		this.searchType = search_type;
+	}
 }
